0bb15c28bcc722f591b20e295d0f7bcd552c6ed9
Tiffany Tang <tifftang22@gmail.com>
2019-10-02 19:46:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,8 +10,8 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array, 2)
-  item1= array.pop
-  item2=array.pop
+def pop_with_args(array)
+  newlist= array.pop
+  newlist += array.pop
   return item1, item2
 end
