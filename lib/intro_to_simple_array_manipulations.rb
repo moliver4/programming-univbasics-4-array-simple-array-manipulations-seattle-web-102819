c9763d17955ebef2316158490202a14ffedd91ac
Tiffany Tang <tifftang22@gmail.com>
2019-10-02 19:58:50 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -39,6 +39,9 @@ def using_flatten(array)
 end
 
 def using_delete(array, string)
+  array.delete(string)
 end
 
-def using_delete_at(array, integeter)
+def using_delete_at(array, integer)
+  array.delete_at(integer)
+end
